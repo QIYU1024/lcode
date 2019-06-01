26aae309747cc9896ded4fe6f4a787711b9e0375
qiyu56 <qiyu56@qq.com>
2019-06-01 17:22:00 +0800

    write/read pipe

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -20,19 +20,49 @@ int main()
 	if(pid == -1)
 		ERR_EXIT("fork error");
 	
+	//if(pid == 0)
+	//{
+	//	close(pipefd[0]);
+	//	int i = 3;
+	//	while(i--)
+	//	{
+	//		write(pipefd[1], "hello", 5);
+	//		sleep(1);
+	//	}
+	//	close(pipefd[1]);
+	//	exit(EXIT_SUCCESS);
+	//}
+	//else
+	//{
+	//	close(pipefd[1]);
+	//	char buf[10] = {0};
+	//	while(1)
+	//	{
+	//		ssize_t val = read(pipefd[0], buf, 10); //当所有管道写端对应的文件描述符被关闭，read返回0
+	//		printf("%s	%u\n", buf, val);
+	//		sleep(1);
+	//	}
+	//}
+	
 	if(pid == 0)
 	{
 		close(pipefd[0]);
-		write(pipefd[1], "hello", 5);
+		int i = 3;
+		while(i--)
+		{
+			write(pipefd[1], "hello", 5);
+		}
 		close(pipefd[1]);
 		exit(EXIT_SUCCESS);
 	}
 	else
 	{
 		close(pipefd[1]);
-		char buf[10] = {0};
-		read(pipefd[0], buf, 10);
-		printf("%s\n", buf);
+		close(pipefd[0]); //关闭管道读端
+		int status;
+		wait(&status);
+		printf("%d\n", status & 0x7F); //低7位，终止信号
+		//当管道读端被关闭，write进程会被SIGPIPE杀死
 	}
 	return 0;
 }
