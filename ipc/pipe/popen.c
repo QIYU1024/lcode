f564583f4e7d99bcb4b2be8c135b52087323a116
qiyu56 <qiyu56@qq.com>
2019-06-03 18:33:01 +0800

    popen

--- ipc/pipe/popen.c
+++ ipc/pipe/popen.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define ERR_EXIT(m) \
+	do\
+	{\
+		perror(m);\
+		exit(EXIT_FAILURE);\
+	}while(0)
 
 int main()
 {
-	
+	FILE *fp = popen("ls -al", "r");
+	if(fp == NULL)
+		ERR_EXIT("popen");
+	int fd = open("file", O_WRONLY | O_CREAT, 0644);
+	if(fd == -1)
+		ERR_EXIT("open");
+	char *buff[1024] = {0};
+	fread(buff, 1024, 1, fp);
+	do
+	{
+		write(fd, buff, sizeof(buff));
+	}while(fread(buff, 1024, 1, fp) != 0);
+	pclose(fp);
+	close(fd);
+	return 0;
 }
+//利用popen()执行命令“ls -al”，将结果输入管道，
+//popen(, "r")返回仅可读的文件指针，读取管道内容到file中
